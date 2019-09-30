106344c8305b217d8e02b979694ec784f08e5eed
Elodie Tran <mcetran@gmail.com>
2019-09-30 19:57:07 +0200

    Challenge done

---
+++ Classroom.java
@@ -0,0 +1,7 @@
+public class Classroom {
+
+    public static void main(String[] args) {
+        Wilder newName = new Wilder("Elodie", false);
+        System.out.println(newName.whoAmI());
+    }
+}
